5374bfc8c0901e650474eceffc64b0aca5133a69
ᴹᵃᵈᵉ ᵒⁿ ᴱᵃʳᵗʰ ᵇʸ ʰᵘᵐᵃⁿˢ <39050604+EXPITC@users.noreply.github.com>
2023-08-03 16:50:29 +0700

    feat: access loop component and specify base id

--- cypress/e2e/home.cy.ts
+++ cypress/e2e/home.cy.ts
@@ -7,9 +7,16 @@ describe("home page", () => {
     })
     it("the h1 contain the correct text", () => {
       cy.visit("http://localhost:3000")
-      cy.get("h1")
+      cy.get(`[data-test="hero-heading"]`)
         .should("be.visible")
         .contains("Testing Next.js Applications with Cypress")
+    })
+
+    it.only("the features on the homepage are correct", () => {
+      cy.visit("http://localhost:3000")
+      cy.get("dt").should("be.visible").eq(0).contains("4 Courses")
+      cy.get("dt").should("be.visible").eq(1).contains("25+ Lessons")
+      cy.get("dt").should("be.visible").eq(2).contains("Free and Open Source")
     })
   })
 })
