dc07818a78cc0675ca4a6d1a8547a743d2c2457c
ᴹᵃᵈᵉ ᵒⁿ ᴱᵃʳᵗʰ ᵇʸ ʰᵘᵐᵃⁿˢ <39050604+EXPITC@users.noreply.github.com>
2023-08-03 17:08:36 +0700

    simplify

--- cypress/e2e/home.cy.ts
+++ cypress/e2e/home.cy.ts
@@ -1,19 +1,16 @@
 describe("home page", () => {
   context("macbook resolution", () => {
     beforeEach(() => {
-      // run these tests as if in a desktop
-      // browser with a 720p monitor
-      cy.viewport("macbook-15")
+      cy.viewport("macbook-15").visit("http://localhost:3000")
     })
+
     it("the h1 contain the correct text", () => {
-      cy.visit("http://localhost:3000")
       cy.get(`[data-test="hero-heading"]`)
         .should("be.visible")
         .contains("Testing Next.js Applications with Cypress")
     })
 
-    it.only("the features on the homepage are correct", () => {
-      cy.visit("http://localhost:3000")
+    it("the features on the homepage are correct", () => {
       cy.get("dt").should("be.visible").eq(0).contains("4 Courses")
       cy.get("dt").should("be.visible").eq(1).contains("25+ Lessons")
       cy.get("dt").should("be.visible").eq(2).contains("Free and Open Source")
